3092953a0c12f0cb364f7d7bea346e5fc6a47d58
EpicCarlito <jameschef2018@gmail.com>
2025-10-22 19:12:07 -0400

    Testing Class

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleOp/Testing.java
@@ -0,0 +1,15 @@
+package org.firstinspires.ftc.teamcode.teleOp;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+
+public class Testing extends OpMode {
+    @Override
+    public void init() {
+
+    }
+
+    @Override
+    public void loop() {
+        // Place your telemetry in here
+    }
+}
